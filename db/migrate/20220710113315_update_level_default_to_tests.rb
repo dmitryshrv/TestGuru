bd31e4828b1d2e4a46203781facc1ef1417b6806
Dmitry Sharov <dmitry@wrbt.ru>
2022-07-10 22:40:33 +0400

    fix: откат всех миграций, сделаны заново в соответствии с комментами

--- db/migrate/20220710113315_update_level_default_to_tests.rb
+++
@@ -1,5 +0,0 @@
-class UpdateLevelDefaultToTests < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default :tests, :level, from: nil, to: 1
-  end
-end
--- db/migrate/20220710180748_create_answers.rb
+++ db/migrate/20220710180748_create_answers.rb
@@ -1,8 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
-      t.string :body
-      t.integer :test_id
+      t.text :body
+      t.boolean :correct, default: false
+      t.references :question, null: false, foreign_key:true
+
       t.timestamps
     end
   end
