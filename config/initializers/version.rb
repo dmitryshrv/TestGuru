846a9e15562f55cf5290333d3c4d9f71e5379658
Dmitry Sharov <dmitry@wrbt.ru>
2022-07-10 15:48:23 +0400

    task 3, migrations

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 module TestGuru
   def current_version
-    VERSION = 0.1.0
+    version = '0.1.0'
   end
 end
---
+++ db/migrate/20220708131811_create_answers.rb
@@ -0,0 +1,9 @@
+class CreateAnswers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :answers do |t|
+      t.string :body
+      t.integer :test_id
+      t.timestamps
+    end
+  end
+end
