cc9be6a47e54d2425dd994c59ccac2d3ea78625a
Dmitry Sharov <dmitry@wrbt.ru>
2022-09-27 19:05:24 +0400

    fix: удаление вопроса

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,8 +4,7 @@ class TestPassage < ApplicationRecord
 
   belongs_to :user
   belongs_to :test
-  belongs_to :current_question, class_name: :Question, optional: true, foreign_key: 'current_question_id'
-
+  belongs_to :current_question, class_name: :Question, optional: true
   before_validation :before_validation_set_first_question, on: :create
   before_update :next_question
 
