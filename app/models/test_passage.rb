c6bb45cdba7a4b1980f1a1522ff67e5d0abf1a06
Dmitry Sharov <dmitry@wrbt.ru>
2022-09-27 18:44:20 +0400

    fix: удаление вопроса

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,7 +4,7 @@ class TestPassage < ApplicationRecord
 
   belongs_to :user
   belongs_to :test
-  belongs_to :current_question, class_name: :Question, optional: true, dependent: :destroy
+  belongs_to :current_question, class_name: :Question, optional: true
 
   before_validation :before_validation_set_first_question, on: :create
   before_update :next_question
---
+++ db/migrate/20220927142347_change_foreign_key_test_passage_to_questions.rb
@@ -0,0 +1,4 @@
+class ChangeForeignKeyTestPassageToQuestions < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
